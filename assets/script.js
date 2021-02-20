ec51ddd98bf44ad0436b822e14515796e59db964
dakota <kotalilyy@gmail.com>
2021-02-20 06:39:47 -0600

    create function for determining winner

--- assets/script.js
+++ assets/script.js
@@ -27,7 +27,13 @@ selectionButtons.forEach(selectionButton => {
 
 function makeSelection(selection) {
     const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
     console.log(computerSelection)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
 }
 
 function randomSelection() {
