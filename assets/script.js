d92e5975af0b0a95ac4b6dd7bd46e85d18a481c8
dakota <kotalilyy@gmail.com>
2021-02-20 06:35:01 -0600

    automate random computer selection

--- assets/script.js
+++ assets/script.js
@@ -20,11 +20,17 @@ const SELECTIONS = [
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
-        const selection = SELECTIONS.find(selection => selection.Name === selectionName)
-        makeSelection(selectionName)
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
     })
 })
 
 function makeSelection(selection) {
-    console.log(selection)
+    const computerSelection = randomSelection()
+    console.log(computerSelection)
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
