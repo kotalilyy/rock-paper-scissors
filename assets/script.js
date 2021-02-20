4246d394aa5c314e59cddef7e74d69ab170dd92c
dakota <kotalilyy@gmail.com>
2021-02-20 06:21:08 -0600

    use a global variable

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌️',
+        beats: 'paper'
+    }
+]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
